11ea6ab9a136f5c256a0de5814b7808cf9255606
NikEvgesha <nikitenkowa.e@yandex.ru>
2023-04-28 10:55:44 +0300

    Добавьте файлы проекта.

---
+++ cdevMod3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cdevMod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Evgeniya";
+            Console.WriteLine("Hello, {0}", name);
+        }
+    }
+}
